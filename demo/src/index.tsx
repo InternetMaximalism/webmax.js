5b6c334e7e24be93b57cff3d25f38157fcd93b02
Kazuki Kasahara <nuderebatsin.roal@gmail.com>
2022-09-23 07:08:01 +0900

    use ui framework for demo app

--- demo/src/index.tsx
+++ demo/src/index.tsx
@@ -1,17 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { ColorModeScript } from "@chakra-ui/react";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { App } from "./App";
+import reportWebVitals from "./reportWebVitals";
+import * as serviceWorker from "./serviceWorker";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
+const container = document.getElementById("root");
+if (!container) throw new Error("Failed to find the root element");
+
+createRoot(container).render(
+  <StrictMode>
+    <ColorModeScript />
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorker.unregister();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
