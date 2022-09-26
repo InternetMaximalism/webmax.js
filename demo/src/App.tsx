1c318539c06eaceb98891e3d654024d528f91734
kazuko <nuderebatsin.roal@gmail.com>
2022-09-27 02:43:16 +0400

    sign message with intmaxwallet (#5)

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -8,7 +8,6 @@ import {
   theme,
 } from "@chakra-ui/react";
 import { ColorModeSwitcher } from "./ColorModeSwitcher";
-import { Logo } from "./Logo";
 import { Form } from "./Form";
 
 export const App = () => (
@@ -17,7 +16,6 @@ export const App = () => (
       <Grid minH="100vh" p={3}>
         <ColorModeSwitcher justifySelf="flex-end" />
         <VStack spacing={8}>
-          <Logo h="40vmin" pointerEvents="none" />
           <Text>
             Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
           </Text>
