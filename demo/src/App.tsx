35b9e1bd09fae184457454c37843efa5a905bda2
Kazuki Kasahara <nuderebatsin.roal@gmail.com>
2022-10-10 09:07:20 +0900

    use intmax logo for demo

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -13,14 +13,16 @@ import {
   VStack,
   HStack,
   Link as ChakraLink,
+  Image,
+  useColorModeValue,
 } from "@chakra-ui/react";
 import { FaGithub } from "react-icons/fa";
-import { AiOutlineGlobal } from "react-icons/ai";
 import { ColorModeSwitcher } from "./ColorModeSwitcher";
 import { Connect } from "./Connect";
 import { SendTransaction } from "./SendTransaction";
 import { SignTransaction } from "./SignTransaction";
 import { SignMessage } from "./SignMessage";
+import logo from "./logo.png";
 
 const links = {
   github: "https://github.com/InternetMaximalism/webmax.js",
@@ -35,27 +37,36 @@ export const App = () => (
         <Flex flexDirection="column">
           <VStack pb={8} spacing={6}>
             <Text as="h1">
-              <Text as="span" display="inline-block" fontFamily="Lexend" fontWeight="extrabold">
+              <Text
+                as="span"
+                color={useColorModeValue("purple.400", "purple.700")}
+                display="inline-block"
+                fontFamily="Lexend"
+                fontWeight="extrabold"
+                pr={3}
+              >
                 Webmax.js
               </Text>
               Demo
             </Text>
             <HStack spacing={10}>
               <ChakraLink isExternal href={links.github}>
-                <FaGithub />
+                <FaGithub fontSize="2rem" />
               </ChakraLink>
               <ChakraLink isExternal href={links.intmaxWallet}>
-                <AiOutlineGlobal />
+                <Box h="2rem" w="2rem">
+                  <Image src={logo} alt="logo" />
+                </Box>
               </ChakraLink>
             </HStack>
             <Text as="h2">make your dapps walletless by webmax.js</Text>
           </VStack>
           <Tabs variant="soft-rounded">
             <TabList justifyContent="center" flexWrap="wrap">
-              <Tab>Connect to Account</Tab>
-              <Tab>Sign Transaction</Tab>
-              <Tab>Send Transaction</Tab>
-              <Tab>Sign Message</Tab>
+              <Tab p={{ base: 4, sm: "auto" }}>Connect to Account</Tab>
+              <Tab p={{ base: 4, sm: "auto" }}>Sign Transaction</Tab>
+              <Tab p={{ base: 4, sm: "auto" }}>Send Transaction</Tab>
+              <Tab p={{ base: 4, sm: "auto" }}>Sign Message</Tab>
             </TabList>
             <TabPanels>
               <TabPanel>
