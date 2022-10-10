73b188d4dca798487186db979fcf6a4f93fb9648
kazuko <nuderebatsin.roal@gmail.com>
2022-10-10 09:09:02 +0900

    demo description (#27)
    * use Lato font
    
    * title changed
    
    * links for github and wallet
    
    * wrap tablist
    
    * use intmax logo for demo

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -10,12 +10,24 @@ import {
   theme,
   Flex,
   Text,
+  VStack,
+  HStack,
+  Link as ChakraLink,
+  Image,
+  useColorModeValue,
 } from "@chakra-ui/react";
+import { FaGithub } from "react-icons/fa";
 import { ColorModeSwitcher } from "./ColorModeSwitcher";
 import { Connect } from "./Connect";
 import { SendTransaction } from "./SendTransaction";
 import { SignTransaction } from "./SignTransaction";
 import { SignMessage } from "./SignMessage";
+import logo from "./logo.png";
+
+const links = {
+  github: "https://github.com/InternetMaximalism/webmax.js",
+  intmaxWallet: "https://www.intmaxwallet.io/",
+};
 
 export const App = () => (
   <ChakraProvider theme={theme}>
@@ -23,15 +35,38 @@ export const App = () => (
       <Grid minH="100vh" p={3}>
         <ColorModeSwitcher justifySelf="flex-end" />
         <Flex flexDirection="column">
-          <Text as="h1" mb={6}>
-            Webmax Demo
-          </Text>
+          <VStack pb={8} spacing={6}>
+            <Text as="h1">
+              <Text
+                as="span"
+                color={useColorModeValue("purple.400", "purple.700")}
+                display="inline-block"
+                fontFamily="Lexend"
+                fontWeight="extrabold"
+                pr={3}
+              >
+                Webmax.js
+              </Text>
+              Demo
+            </Text>
+            <HStack spacing={10}>
+              <ChakraLink isExternal href={links.github}>
+                <FaGithub fontSize="2rem" />
+              </ChakraLink>
+              <ChakraLink isExternal href={links.intmaxWallet}>
+                <Box h="2rem" w="2rem">
+                  <Image src={logo} alt="logo" />
+                </Box>
+              </ChakraLink>
+            </HStack>
+            <Text as="h2">make your dapps walletless by webmax.js</Text>
+          </VStack>
           <Tabs variant="soft-rounded">
-            <TabList justifyContent="center">
-              <Tab>Connect to Account</Tab>
-              <Tab>Sign Transaction</Tab>
-              <Tab>Send Transaction</Tab>
-              <Tab>Sign Message</Tab>
+            <TabList justifyContent="center" flexWrap="wrap">
+              <Tab p={{ base: 4, sm: "auto" }}>Connect to Account</Tab>
+              <Tab p={{ base: 4, sm: "auto" }}>Sign Transaction</Tab>
+              <Tab p={{ base: 4, sm: "auto" }}>Send Transaction</Tab>
+              <Tab p={{ base: 4, sm: "auto" }}>Sign Message</Tab>
             </TabList>
             <TabPanels>
               <TabPanel>
--- demo/src/index.tsx
+++ demo/src/index.tsx
@@ -4,6 +4,7 @@ import { createRoot } from "react-dom/client";
 import { App } from "./App";
 import reportWebVitals from "./reportWebVitals";
 import * as serviceWorker from "./serviceWorker";
+import "./index.css";
 
 const container = document.getElementById("root");
 if (!container) throw new Error("Failed to find the root element");
