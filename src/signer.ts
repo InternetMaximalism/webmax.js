67bd57043cee893945fccc7a0ddbb3bb875999b1
Akira-Taniguchi <dededededaiou2003@yahoo.co.jp>
2023-06-07 16:00:42 +0900

    transfer to English

--- src/signer.ts
+++ src/signer.ts
@@ -246,10 +246,9 @@ export class IntmaxWalletSigner {
           if (!data.result) {
             return reject(data.message as string);
           }
-          // signature、EVMの場合はstringだが、StarkNetの場合はstring[]
-          // いずれchain idなどのメタ情報も併せてウォレットから戻してきた方がいいかも。
-          // チェーンによってインターフェースが違うので、戻り値の型を見て判断するというのは
-          // ちょっとスマートじゃない
+          // The type of the value of the signature, a string in the case of EVM, but a string[] in the case of StarkNet.
+          // It might be a good idea to eventually get the chain id and other meta information back from the wallet as well.
+          // Since different chains have different interfaces, I don't think it is a good idea to judge by looking at the return type.
           resolve(event.data.message as T);
         }
       };
